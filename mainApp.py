6a397fb706a3b3f04bcffaac85c51c52770c9d61
Alan Gładyś <alanekgladys117@gmail.com>
2026-01-19 18:53:08 +0100

    Program wymaga testowania

--- mainApp.py
+++ mainApp.py
@@ -5,9 +5,8 @@ import functions
 
 print("Matrix dla opornych - program do wykonywania podstawowych działań na macierzach.")
 input()
-A = [[2,1,0],[1,1,2]]
-B = [[3,1],[3,-2],[-3,5]]
+B = [[2,1,0],[1,1,2]]
+A = [[3,1],[3,-2],[-3,5]]
 
-functions.sum_matrices(A, B)
-functions.sum_matrices(A, B)
-functions.sub_matrices(A, B)
+functions.multiply_matrices(A, B)
+functions.divide_matrices(A, B)
